2f2ad086365ef024c5b89f9056c1a22a8daa4f27
vmedina <veromedina22@gmai.com>
2024-08-14 10:34:52 -0300

    se evaluan dos listas para determinar si comparten algun elemento

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 def evaluar_listas(mi_lista_uno, mi_lista_dos):   
-    #con split se puede corta cada palabra para generar un arreglo.
+    # itero a través de una lista y verifico si cada elemento está presente en la otra lista.
     for elemento in mi_lista_uno:
         if elemento in mi_lista_dos:
             return True
     return False
 
-#se define un string cualquiera 
+#se defined dos listas 
 mi_lista_uno = [1,3,5,6]
 mi_lista_dos = [12,4, 7, 10]
 
-#la funcion manipular cadena recibe 
-
-
+#la funcion evaluar_listas recibe dos listas y si comparten elementos retorna true
 print(evaluar_listas(mi_lista_uno, mi_lista_dos))
