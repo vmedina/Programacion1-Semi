326ae31b4a8a820c9db45f8f98874b99d23a68d8
vmedina <veromedina22@gmai.com>
2024-08-31 18:01:56 -0300

    encuentra las palabras con uso mas comun y grafica el resultado

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 # main.py
+import collections as col
+import matplotlib.pyplot as plt
 
-# Importar funciones del módulo text_formatting
-from text_formating import a_negrita, a_italica, subrayado, texto_normal
 
+
+
+def analizadorDeTexto(textoParaAnalizar):
+    if not textoParaAnalizar :
+        print("El texto no es válido")
+    else:
+        #Counter es una subclase de dict que cuenta la frecuencia de los elementos. 
+        #Almacena pares clave-valor donde la clave es el elemento de la colección 
+        # y el valor es la cantidad de veces que ese elemento aparece
+        #El método most_common(n) de un objeto Counter devuelve una lista de los n elementos más comunes 
+        # y sus frecuencias en orden descendente
+        
+        palabrasMasComunes = col.Counter(textoParaAnalizar).most_common(10)  
+        #llama a la función que genera la grafica y le pasa como parametro las palabras más usadas. 
+        graficarDatos(palabrasMasComunes)
+    
+
+def graficarDatos(data):
+    
+    # Separa los datos en dos listas: palabras y frecuencias
+    words, frequencies = zip(*data)
+
+    # Crear la gráfica de barras con 10 pulgadas de ancho y 6 de alto 
+    plt.figure(figsize=(10, 6))
+    
+    # words se utiliza para las etiquetas del eje X 
+    # frequencies para la altura de las barras en el eje Y. 
+    # El color de las barras se establece en 'skyblue'.
+    plt.bar(words, frequencies, color='skyblue')
+
+    # Añadir título y etiquetas
+    plt.title('Frecuencia de palabras')
+    plt.xlabel('Palabras')
+    plt.ylabel('Frecuencia')
+
+    # Mostrar la gráfica
+    plt.show()
+    
+    
 # Texto de ejemplo
-texto = "Hola Mundo"
+texto = "Python es un lenguaje de alto nivel de programación interpretado cuya filosofía hace hincapié en la legibilidad de su código. Se trata de un lenguaje de programación multiparadigma, ya que soporta parcialmente la orientación a objetos, programación imperativa y, en menor medida, programación funcional."
+# llamo a la función que analiza el texto 
+print(analizadorDeTexto(texto.split(" ")))
 
-# Aplicar formatos
-texto_negrita = a_negrita(texto)
-texto_italica = a_italica(texto)
-texto_subrayado = subrayado(texto)
-texto_sin_formato = texto_normal(texto)
-
-# Imprimir resultados
-print("Texto en negrita:", texto_negrita)
-print("Texto en cursiva:", texto_italica)
-print("Texto subrayado:", texto_subrayado)
-print("Texto normal:", texto_sin_formato)
